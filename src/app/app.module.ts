f5c0496cc353e2b23c67e67b3e99f659f5b77aaa
ayoussef0900 <a.youssef@sieraj.com>
2021-11-23 09:24:19 +0200

    angular-setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,19 +9,10 @@ import { HttpClientModule } from '@angular/common/http';
 import { TranslateModule } from '@ngx-translate/core';
 import { SharedModule } from '@shared';
 import { HeaderComponent } from './header/header.component';
-import { PostComponent } from './post/post.component';
-import { MainPostsComponent } from './main-posts/main-posts.component';
-import { ListPostComponent } from './list-post/list-post.component';
-import { PostDetailsComponent } from './post-details/post-details.component';
-
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    PostComponent,
-    MainPostsComponent,
-    ListPostComponent,
-    PostDetailsComponent,
   ],
   imports: [
     BrowserModule,
